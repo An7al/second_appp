06bc2f7c1d10665f9665eabac813a118ac22de5d
Ali Saka <6dksdsda@duck.com>
2024-01-18 10:36:35 +0100

    method calls outside of class

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -30,7 +30,7 @@ class _StartScreenState extends State<StartScreen> {
         ),
         OutlinedButton.icon(
           onPressed: () {
-            widget.switchScreen(); //
+            switchScreen(); //
           },
           label: const Text("Start Quiz",
           ),
