8bb0516514fb029bee83ebae0872c70ed1d462ab
Ali Saka <6dksdsda@duck.com>
2024-01-18 10:30:29 +0100

    keyword late to avoid use before initialise

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
-  const StartScreen({
-    super.key,
-  });
+class StartScreen extends StatefulWidget {
+  StartScreen(this.switchScreen, {super.key,});
+  void Function() switchScreen; // instantiating other class's method with constructor to call it  unlike calling ClassObject.method() or making it static with Class.method()
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
 
+class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(mainAxisSize: MainAxisSize.min, children: [
         Image.asset(
           'assets/images/quiz-logo.png',
-          width: 300,
+          width: 250,
           color: Colors.white,
           // fit: BoxFit.fill,
         ),
@@ -20,16 +23,17 @@ class StartScreen extends StatelessWidget {
         ),
         const Text(
           "Learn Flutter the fun way",
-          selectionColor: Colors.white,
+          style: TextStyle(color: Colors.white),
         ),
         const SizedBox(
           height: 25,
         ),
         OutlinedButton.icon(
           onPressed: () {
-            // next screen
+            widget.switchScreen(); //
           },
-          label: const Text("Start Quiz"),
+          label: const Text("Start Quiz",
+          ),
           icon: const Icon(Icons.arrow_drop_up_outlined),
         )
       ]),
