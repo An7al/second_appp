6e058d166917eaa7ff7d01a5c14effd8e2817968
Ali Saka <6dksdsda@duck.com>
2024-01-27 16:39:28 +0100

    SizedBox wrap with doubled affinity instead of Center

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/data/questions.dart';
 
-class QuestionsScreen extends StatelessWidget {
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var pageNumber = 0;
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SizedBox(
+      width: double.infinity, // Box gets the whole width of screen
+      child: Column(
+        children: [
+          Text(questions[pageNumber].question),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+              onPressed: () {}, child: Text(questions[pageNumber].answers[0])),
+          ElevatedButton(
+              onPressed: () {}, child: Text(questions[pageNumber].answers[1])),
+        ],
+      ),
+    );
   }
 }
